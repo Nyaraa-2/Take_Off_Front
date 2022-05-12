a0bb7d52004a7d9cb3a51e977a92065675dcdf8c
Nyaraa-2 <marina.vandenbosch@outlook.fr>
2022-05-12 18:11:27 +0200

    :sparkles: edit custom input & button => emit event

--- src/components/SearchComponent/SearchBanner.vue
+++ src/components/SearchComponent/SearchBanner.vue
@@ -1,26 +1,49 @@
 <template>
   <div class="flex flex-row my-6 mx-7 gap-5 items-center">
-    <input
-      class="border-1 border-bg-gray-400 p-3 bg-red transition h-10 rounded-md focus:outline-none w-1/3 text-black text-lg"
-      type="search"
-      name="search"
-      placeholder="Ex : Développeur Fullstack"
+    <CustomInput
+      name="job"
+      type="text"
+      placeholder="Ex : DevOps"
+      label="Job recherché"
+      value="searchJob"
+      v-model="searchJobValue"
+      class="w-1/3"
+      v-on:keyup.enter="submit"
     />
-    <input
-      class="border-1 border-bg-gray-400 p-3 bg-red transition h-10 rounded-md focus:outline-none w-1/3 text-black text-lg"
-      type="search"
-      name="search"
+    <CustomInput
+      name="city"
+      type="text"
       placeholder="Ex : Lille"
+      label="Ville recherchée"
+      value="searchCity"
+      v-model="searchCityValue"
+      class="w-1/3"
+      v-on:keyup.enter="submit"
     />
-    <LayoutButton class="w-1/3">Rechercher</LayoutButton>
+
+    <LayoutButton @submit="click" action="search" primary class="w-1/3"
+      >Rechercher</LayoutButton
+    >
   </div>
 </template>
 
 <script>
 import LayoutButton from '../CommonComponent/LayoutButton.vue'
+import CustomInput from '@/components/CommonComponent/CustomInput.vue'
 export default {
   name: 'SearchBanner',
-  components: { LayoutButton },
+  components: { LayoutButton, CustomInput },
+  data() {
+    return {
+      searchJobValue: '',
+      searchCityValue: '',
+    }
+  },
+  methods: {
+    click(action) {
+      console.log(action)
+    },
+  },
 }
 </script>
 
